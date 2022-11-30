c771758d96fc120f4ae0c6118f43b72a5b1185a2
PocketMiner82 <73847326+PocketMiner82@users.noreply.github.com>
2022-11-30 14:25:11 +0100

    Add files via upload

--- MultiplayerSnake/Form1.Designer.cs
+++ MultiplayerSnake/Form1.Designer.cs
@@ -28,31 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pnSpielfeld = new System.Windows.Forms.Panel();
+            this.pbGame = new System.Windows.Forms.PictureBox();
+            this.lbStatus = new System.Windows.Forms.Label();
+            this.lbSidebar = new System.Windows.Forms.Label();
+            this.btnRetry = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGame)).BeginInit();
             this.SuspendLayout();
             // 
-            // pnSpielfeld
+            // pbGame
             // 
-            this.pnSpielfeld.Location = new System.Drawing.Point(0, 0);
-            this.pnSpielfeld.Name = "pnSpielfeld";
-            this.pnSpielfeld.Size = new System.Drawing.Size(775, 403);
-            this.pnSpielfeld.TabIndex = 0;
+            this.pbGame.Location = new System.Drawing.Point(311, 89);
+            this.pbGame.Name = "pbGame";
+            this.pbGame.Size = new System.Drawing.Size(477, 269);
+            this.pbGame.TabIndex = 1;
+            this.pbGame.TabStop = false;
             // 
-            // MultiplayerSnake
+            // lbStatus
+            // 
+            this.lbStatus.AutoSize = true;
+            this.lbStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbStatus.Location = new System.Drawing.Point(0, 0);
+            this.lbStatus.Name = "lbStatus";
+            this.lbStatus.Size = new System.Drawing.Size(228, 54);
+            this.lbStatus.TabIndex = 2;
+            this.lbStatus.Text = "Loading...";
+            // 
+            // lbSidebar
+            // 
+            this.lbSidebar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSidebar.Location = new System.Drawing.Point(6, 54);
+            this.lbSidebar.Name = "lbSidebar";
+            this.lbSidebar.Padding = new System.Windows.Forms.Padding(0, 35, 0, 0);
+            this.lbSidebar.Size = new System.Drawing.Size(194, 1000);
+            this.lbSidebar.TabIndex = 3;
+            this.lbSidebar.Text = "Online: 1/15\r\n\r\ntest: 23";
+            // 
+            // btnRetry
+            // 
+            this.btnRetry.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnRetry.Location = new System.Drawing.Point(12, 426);
+            this.btnRetry.Name = "btnRetry";
+            this.btnRetry.Size = new System.Drawing.Size(75, 23);
+            this.btnRetry.TabIndex = 4;
+            this.btnRetry.Text = "Retry";
+            this.btnRetry.UseVisualStyleBackColor = true;
+            this.btnRetry.Visible = false;
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.pnSpielfeld);
-            this.Name = "MultiplayerSnake";
+            this.ClientSize = new System.Drawing.Size(834, 461);
+            this.Controls.Add(this.btnRetry);
+            this.Controls.Add(this.lbSidebar);
+            this.Controls.Add(this.pbGame);
+            this.Controls.Add(this.lbStatus);
+            this.MinimumSize = new System.Drawing.Size(850, 500);
+            this.Name = "MainForm";
             this.Text = "Multiplayer Snake";
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pbGame)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel pnSpielfeld;
+        private System.Windows.Forms.PictureBox pbGame;
+        private System.Windows.Forms.Label lbStatus;
+        private System.Windows.Forms.Label lbSidebar;
+        private System.Windows.Forms.Button btnRetry;
     }
 }
 
---
+++ MultiplayerSnake/FullScreen.cs
@@ -0,0 +1,61 @@
+﻿using System.Windows.Forms;
+
+namespace MultiplayerSnake
+{
+    class FullScreen
+    {
+        Form TargetForm;
+
+        FormWindowState PreviousWindowState;
+
+        bool fullScreenEnabled = false;
+
+        public FullScreen(Form targetForm)
+        {
+            TargetForm = targetForm;
+            TargetForm.KeyDown += TargetForm_KeyDown;
+        }
+
+        private void TargetForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.F11)
+            {
+                Toggle();
+            }
+        }
+
+        public void Toggle()
+        {
+            if (fullScreenEnabled)
+            {
+                Leave();
+            }
+            else
+            {
+                Enter();
+            }
+        }
+
+        public void Enter()
+        {
+            if (!fullScreenEnabled)
+            {
+                PreviousWindowState = TargetForm.WindowState;
+                TargetForm.WindowState = FormWindowState.Normal;
+                TargetForm.FormBorderStyle = FormBorderStyle.None;
+                TargetForm.WindowState = FormWindowState.Maximized;
+                fullScreenEnabled = true;
+            }
+        }
+
+        public void Leave()
+        {
+            if (TargetForm.WindowState == FormWindowState.Maximized && fullScreenEnabled)
+            {
+                TargetForm.FormBorderStyle = FormBorderStyle.Sizable;
+                TargetForm.WindowState = PreviousWindowState;
+                fullScreenEnabled = false;
+            }
+        }
+    }
+}
