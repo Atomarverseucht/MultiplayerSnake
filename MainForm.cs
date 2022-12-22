d26789248b843ec73348f65bde4a7b9291c78d23
Atom <100242617+Atomarverseucht@users.noreply.github.com>
2022-12-22 15:12:18 +0100

    Timer start

--- MainForm.cs
+++ MainForm.cs
@@ -104,7 +104,7 @@ namespace MultiplayerSnake
             {
                 Application.Exit();
             }
-
+            tmUpdate.Start();
             name = InputBox.ResultValue;
             return;
         }
