55892621797ea0c769aa1f8b45be9f9d04b081cc
PocketMiner82 <pocket.miner@gmx.de>
2023-01-06 17:25:53 +0100

    put highscores together with player names

--- HighscoresForm.cs
+++ HighscoresForm.cs
@@ -12,7 +12,8 @@ namespace MultiplayerSnake
 
         private readonly string[] colors = new string[] { "gold", "silver", "chocolate", "black" };
 
-        int firstScore = 100; // max. Score
+        // max. Score
+        int firstScore = 100;
 
         public HighscoresForm(Dictionary<string, int> scores)
         {
@@ -34,25 +35,18 @@ namespace MultiplayerSnake
         }
 
 
-        // Methods for the BarGraph()
         public void drawBarChart(Graphics g, int score, int yposition, string p_name, Brush b)
         {
-            g.DrawString(p_name, new Font("Arial", 12), Brushes.Black, 10, yposition);                                              // displays the name
-            g.FillRectangle(b, 0, yposition+20, calculateBar(score), 5);                                                            // Bargraph
-            if (calculateBar(score) > 100)
-            {
-                g.DrawString(score.ToString(), new Font("Arial", 12), Brushes.DarkGray, calculateBar(score) + 10, yposition + 12);      // displays the score
-            }
-            else
-            {
-                g.DrawString(score.ToString(), new Font("Arial", 12), Brushes.DarkGray, 110, yposition + 12);
-            }
-
+            // displays the name
+            g.DrawString(p_name + ": " + score, new Font("Arial", 12), Brushes.Black, 10, yposition);
+            // Bargraph
+            g.FillRectangle(b, 0, yposition+20, calculateBar(score), 5);
         }
 
         public int calculateBar(int score)
         {
-            return score * (Width-100) / firstScore;   // Score * max. length / max. score
+            // Score * max. length / max. score
+            return score * (Width-100) / firstScore;
         }
 
         private void btBack_Click(object sender, EventArgs e)
@@ -64,6 +58,7 @@ namespace MultiplayerSnake
         {   
             
             e.Graphics.Clear(SystemColors.Control);
+            // if there are no scores, tell the player by writing it on the center of the PB
             if (!scores.Any())
             {
                 StringFormat sf = new StringFormat();
@@ -81,6 +76,7 @@ namespace MultiplayerSnake
             {
                 int scorePlayer = score.Value;
                 string namePlayer = score.Key;
+                // add the specific highscore + player name
                 drawBarChart(e.Graphics, scorePlayer, i * 50 + 20, namePlayer, new SolidBrush(Color.FromName(colors[Math.Min(i, 3)])));
                 i++;
             }
